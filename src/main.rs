da34babd34671aeacbbd3b602ef89f37ac3221bc
Elizbeth <elizabethadebayo269@gmail.com>
2024-03-18 15:12:28 +0100

    created  a struct that describes the features of a rectangle and i created a function that calculates the are of a rectangle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+struct Rectangle {
+    height: u32,
+    breadth: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle {
+        height: 30,
+        breadth: 40,
+    };
+    println!("the area of the rectangle is {} square unit", area(&rect))
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.breadth
 }
